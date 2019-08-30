d911da4b73cbb38b3d7d66eed9eb695c3fc86c83
Nicolas Leclerc <nleclerc@sodifrance.fr>
2019-08-30 17:47:50 +0200

    ajout du tableau de post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+interface Post {
+  title: string;
+  content: string;
+  loveIts: number;
+  created_at: Date;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'activite1';
+  posts: Post[] = [
+    {
+      title: 'Mon premier post',
+      content: 'In hac habitasse platea dictumst. Aenean rutrum porta ipsum, et consequat metus efficitur vitae. ' +
+        'Sed vitae imperdiet lacus. Nam leo justo, faucibus in tristique quis, euismod at augue.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Proin arcu lectus, accumsan vel eleifend quis, venenatis nec massa. Donec mattis faucibus mauris, ' +
+        'ut tincidunt lacus feugiat a. Sed in diam lacinia, fermentum nibh vel, rhoncus metus.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Encore un post',
+      content: 'Curabitur eleifend quis lorem ut efficitur. Fusce pretium varius leo ac ornare. ' +
+        'Proin consectetur sapien et euismod suscipit. Maecenas pretium tellus et lacinia finibus.',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
